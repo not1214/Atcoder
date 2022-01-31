2a564713f85d4c46df56d3d177a12874001c8dca
not1214 <91748221+not1214@users.noreply.github.com>
2022-01-31 19:37:35 +0900

    Atcoder 高橋君とカード

--- 高橋君とカード.rb
+++ 高橋君とカード.rb
@@ -7,3 +7,32 @@
 # array.each do |a|
 
 # end
+
+N, A = gets.to_s.split.map{|t|t.to_i}
+array = gets.to_s.split.map{|t|t.to_i - A}.sort.reverse
+
+# p N
+# p A
+# p array
+ 
+hash = Hash.new(0)
+hash[0] = 1
+# p hash
+
+N.times do |i|
+  
+  t = array[i]
+  # p t
+  N = hash.dup
+  # p N
+  hash.keys.each do |k|
+    # p k
+    N[k+t] += hash[k]
+  end
+  # p N
+  hash = N.dup
+  p hash
+  
+end
+# p hash
+# puts hash[0] - 1
