54099906e518217cbd370b20ed8db43e4c974b96
not1214 <91748221+not1214@users.noreply.github.com>
2022-02-03 20:09:21 +0900

    Atcoder AtCoder *** Contest

--- AtCoderContest.rb
+++ AtCoderContest.rb
@@ -1,4 +1,11 @@
-arr = gets.chomp.split(' ').map(&:to_s)
-# p arr
+# arr = gets.chomp.split(' ').map(&:to_s)
+# # p arr
 
-puts "#{arr[0][0]}#{arr[1][0]}#{arr[2][0]}"
+# puts "#{arr[0][0]}#{arr[1][0]}#{arr[2][0]}"
+
+
+arr = []
+while ture do
+  arr << gets.split(' ').map(&:to_s)
+end
+p arr
