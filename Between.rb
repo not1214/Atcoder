5483b518b06c0dd180c822a4c7170bb88720a9e9
not1214 <91748221+not1214@users.noreply.github.com>
2022-02-04 17:33:17 +0900

    Atcoder Between a and b ...

--- Between.rb
+++ Between.rb
@@ -1,1 +1,18 @@
-git 
+a, b, x = gets.chomp.split(' ').map(&:to_i)
+# arr = []
+
+# for i in a..b do
+#   if i % x == 0
+#     arr << i
+#   end
+# end
+
+# puts arr.size
+
+if a == 0
+    puts b / x + 1
+else
+    result = b / x - a / x
+    result += 1 if a % x == 0
+    puts result
+end
