6cd2179a2962742f14e126242a417a6c3648f90f
not1214 <91748221+not1214@users.noreply.github.com>
2022-02-06 16:41:14 +0900

    Atcoder ContestwithDrinksEasy

--- ContestwithDrinksEasy.rb
+++ ContestwithDrinksEasy.rb
@@ -1,16 +1,26 @@
-N = gets.chomp.to_i
-arr = gets.chomp.split(' ').map(&:to_i)
-# p arr.size
-M = gets.chomp.to_i
+# N = gets.chomp.to_i
+# arr = gets.chomp.split(' ').map(&:to_i)
+# # p arr.size
+# M = gets.chomp.to_i
 
+# M.times do
+#   arr2 = arr.dup
+#   result = 0
+#   P, X = gets.chomp.split(' ').map(&:to_i)
+#   arr2[P-1] = X
+#   # p arr
+#   for i in 0..(arr.size-1) do
+#     result += arr2[i]
+#   end
+#   puts result
+# end
+
+N = gets.to_i
+t = gets.split(" ").map(&:to_i)
+M = gets.to_i
+sum = t.sum
+ 
 M.times do
-  arr2 = arr.dup
-  result = 0
-  P, X = gets.chomp.split(' ').map(&:to_i)
-  arr2[P-1] = X
-  # p arr
-  for i in 0..(arr.size-1) do
-    result += arr2[i]
-  end
-  puts result
+  index, newTime = gets.split(" ").map(&:to_i)
+  puts sum - t[index - 1] + newTime
 end
