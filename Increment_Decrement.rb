1b9b98a278ed033fdd7b631a761ac7ee57792621
not1214 <91748221+not1214@users.noreply.github.com>
2022-02-08 14:52:48 +0900

    Atcoder Increment_Decrement

---
+++ Increment_Decrement.rb
@@ -0,0 +1,19 @@
+N = gets.chomp.to_i
+S = gets.chomp.split('').map(&:to_s)
+X = 0
+result = []
+
+N.times do |i|
+  if S[i] == "I"
+    X += 1
+  elsif S[i] == "D"
+    X -= 1
+  end
+  result << X
+end
+
+if result.max >= 0
+  puts result.max
+else
+  puts 0
+end
