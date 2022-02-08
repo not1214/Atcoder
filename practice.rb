7c3a06748d5360f2942462d88bad01fe22d7c0b9
not1214 <91748221+not1214@users.noreply.github.com>
2022-02-09 02:03:25 +0900

    お手伝い

---
+++ practice.rb
@@ -0,0 +1,20 @@
+arr = [
+        [{"title"=>"test1","user"=>"ogu"},{"title"=>"test2","user"=>"nao"}],
+        [{"title"=>"test3","user"=>"hoge"},{"title"=>"test4","user"=>"gon"}],
+        [{"title"=>"test5","user"=>"taro"},{"title"=>"test6","user"=>"jiro"}]
+                                                                              ]
+
+# p arr[0][0]["title"]
+# p arr.size
+# p arr[0].size
+test = []
+# arr.each do |val|
+for val in arr do
+  arr[0].size.times do |i|
+    # p val
+    test << val[i]["title"]
+    # p test
+  end
+end
+
+p test
