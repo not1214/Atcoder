5ea9199d742d9faa397b9d6c33f135b588c01e6f
not1214 <91748221+not1214@users.noreply.github.com>
2022-02-09 18:29:25 +0900

    お手伝い

--- practice.rb
+++ practice.rb
@@ -1,20 +1,30 @@
-arr = [
-        [{"title"=>"test1","user"=>"ogu"},{"title"=>"test2","user"=>"nao"}],
-        [{"title"=>"test3","user"=>"hoge"},{"title"=>"test4","user"=>"gon"}],
-        [{"title"=>"test5","user"=>"taro"},{"title"=>"test6","user"=>"jiro"}]
-                                                                              ]
+# arr = [
+#         [{"title"=>"test1","user"=>"ogu"},{"title"=>"test2","user"=>"nao"}],
+#         [{"title"=>"test3","user"=>"hoge"},{"title"=>"test4","user"=>"gon"}],
+#         [{"title"=>"test5","user"=>"taro"},{"title"=>"test6","user"=>"jiro"}]
+#                                                                               ]
 
-# p arr[0][0]["title"]
-# p arr.size
-# p arr[0].size
-test = []
-# arr.each do |val|
-for val in arr do
-  arr[0].size.times do |i|
-    # p val
-    test << val[i]["title"]
-    # p test
-  end
+# # p arr[0][0]["title"]
+# # p arr.size
+# # p arr[0].size
+# test = []
+# # arr.each do |val|
+# for val in arr do
+#   arr[0].size.times do |i|
+#     # p val
+#     test << val[i]["title"]
+#     # p test
+#   end
+# end
+
+# p test
+
+datas = []
+2.times do  
+  datas.push({
+    name: "aaa",
+    foo: "test"
+  })
 end
 
-p test
+p datas
