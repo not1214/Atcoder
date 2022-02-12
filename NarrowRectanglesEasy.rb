5a2fb1267b41fcd72f33ac232f2781c3cadf3592
not1214 <91748221+not1214@users.noreply.github.com>
2022-02-12 14:36:20 +0900

    Atcoder NarrowRectanglesEasy

--- NarrowRectanglesEasy.rb
+++ NarrowRectanglesEasy.rb
@@ -12,3 +12,19 @@ if result <= 0
 else
   puts result
 end
+
+# w, a, b = gets.split.map(&:to_i)
+ 
+# if a > b
+#   if b+w >= a
+#     puts '0'
+#   else
+#     puts a - (b + w)
+#   end
+# elsif a < b
+#   if a+w >= b
+#     puts '0'
+#   else
+#     puts b - (a + w)
+#   end
+# else
