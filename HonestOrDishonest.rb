11be6fc729a6d78cf60837e30c4fa62272b242e1
not1214 <91748221+not1214@users.noreply.github.com>
2022-02-12 14:20:59 +0900

    Atcoder HonestOrDishonest

--- HonestOrDishonest.rb
+++ HonestOrDishonest.rb
@@ -1,11 +1,19 @@
-a, b = gets.chomp.split(' ').map(&:to_s)
+# a, b = gets.chomp.split(' ').map(&:to_s)
 
-if a == "H" && b == "H"
+# if a == "H" && b == "H"
+#   puts "H"
+# elsif a == "H" && b == "D"
+#   puts "D"
+# elsif a == "D" && b == "H"
+#   puts "D"
+# elsif a == "D" && b == "D"
+#   puts "H"
+# end
+
+arr = gets.split(" ").map(&:to_s)
+
+if arr[0] == arr[1]
   puts "H"
-elsif a == "H" && b == "D"
+else
   puts "D"
-elsif a == "D" && b == "H"
-  puts "D"
-elsif a == "D" && b == "D"
-  puts "H"
 end
