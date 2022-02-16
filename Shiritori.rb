542748086c5a850133efa00261e591d011765e55
not1214 <91748221+not1214@users.noreply.github.com>
2022-02-16 18:03:28 +0900

    Atcoder Shiritori

--- Shiritori.rb
+++ Shiritori.rb
@@ -1,10 +1,18 @@
+# A, B, C = gets.chomp.split(' ').map(&:to_s)
+
+# A = A.split('')
+# B = B.split('')
+# C = C.split('')
+
+# if A.last == B.first && B.last == C.first
+#   puts "YES"
+# else
+#   puts "NO"
+# end
+
 A, B, C = gets.chomp.split(' ').map(&:to_s)
 
-A = A.split('')
-B = B.split('')
-C = C.split('')
-
-if A.last == B.first && B.last == C.first
+if A[-1] == B[0] && B[-1] == C[0]
   puts "YES"
 else
   puts "NO"
